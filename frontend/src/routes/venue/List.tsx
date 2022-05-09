a6ac46cf83ce453d3813e93f08d1a97b4d81af81
John Johnson <john@the1johnson.com>
2022-05-09 11:47:21 -0700

    Venues list matches bands list

--- frontend/src/routes/venue/List.tsx
+++ frontend/src/routes/venue/List.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Outlet, Link } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
 import GenericWrapper from '../../components/GenericWrapper';
-import { Outlet } from "react-router-dom";
+import { setVenues } from '../../features/venuesSlice';
 
 export default function VenuesList() {
+    const dispatch = useDispatch();
+    const venues = useSelector((state: any) => state.venues.venue_list);
+    function updateVenuesList() {
+        const allVenuesApiUrl = `${process.env.REACT_APP_AJAX_URL}/all_venues`;
+        const getData = async () => {
+            try {
+                const response = await fetch(
+                    allVenuesApiUrl,
+                    {
+                        method: 'GET',
+                        headers: { 'Content-Type': 'application/json' },
+                    }
+                );
+
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+                let actualData = await response.json();
+                dispatch(setVenues(actualData));
+            } catch (err: any) {
+
+            }
+        }
+        getData();
+    }
+    useEffect(() => {
+        updateVenuesList();
+    }, []);
     return (
         <GenericWrapper>
-            <div>
-                All Venues
+            <h2 className='text-lg font-bold tracking-wide'>Venues List</h2>
+            <div className='flex grid grid-cols-2'>
+                <ul>
+                    {venues.map((venue: any) => {
+                        return <li key={venue.id}><Link to={`/venues/${venue.id}`}>{venue.name}</Link></li>;
+                    })}
+                </ul>
                 <Outlet />
             </div>
         </GenericWrapper>
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/authSlice';
 import bandsReducer from './features/bandsSlice';
+import venuesReducer from './features/venuesSlice';
 
 export default configureStore({
   reducer: {
       auth: authReducer,
-      bands: bandsReducer
+      bands: bandsReducer,
+      venues: venuesReducer
   },
 });
